67b4dd9dbd67d1c4044ae973fee61299bbb0a47b
meistens <mebodave@gmail.com>
2022-10-23 02:24:42 +0100

    code refactor and practice

---
+++ linked_list/practice/1-intro.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * description: linked list structure
+ * @data: data of the node
+ * @next: address store of the node
+ *
+ */
+
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
+
+/**
+ * description: singly linked list demo
+ * @elem1: linked list
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[]) {
+
+  Node elem1;
+  elem1.data = 1;
+  elem1.next = malloc(sizeof(Node));
+  elem1.next->data = -2;
+  elem1.next->next = malloc(sizeof(Node));
+  elem1.next->next->data = 45;
+  elem1.next->next->next = NULL;
+
+  /**
+   * iterating over the list
+   */
+  for (Node *current = &elem1; current != NULL; current = current->next) {
+    printf("%d\n", current->data);
+  }
+
+  free(elem1.next->next);
+  free(elem1.next);
+  return 0;
+}
